ed57b322c3011a4f258988f862a04d56b72e5267
gersondani2001 <gersonboche01@gmail.com>
2019-06-01 10:02:29 -0600

    api

--- js/script.js
+++ js/script.js
@@ -14,8 +14,19 @@ $(document).ready(function(){
 */
 
 
-var app = angular.module("app",[])
-app.controller("appCtrl",function($scope,$rootScope){
+var app = angular.module("pokeapilite",[])
+app.controller("appCtrl",function($scope,$rootScope,$http){
 	console.log("Angular.js")
 	console.log("JavaScript.js")
+
+	$scope.Pokeapi = [];
+	for (var x = 1; x < 500; x++) {
+		$http({
+			method : "GET",
+			url : "https://pokeapi.co/api/v2/pokemon/" + x
+		}).then(function(snapshot){
+			$scope.Pokeapi.push(snapshot);
+		})		
+		console.log($scope.Pokeapi);
+	}
 });
